3f9d83cd5731e67b2c670d402a4e559f20be312c
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-01-23 19:24:48 -0500

    Initial commit

---
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -0,0 +1,18 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.entrypoint.EntrypointTransformer;
+import net.fabricmc.loader.launch.common.FabricLauncherBase;
+import java.lang.reflect.InvocationTargetException;
+
+public class ExampleMod implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		System.out.println("Hello Fabric world!");
+	}
+}
